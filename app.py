83067dc5eb8bb7d72bd2ed03969cfcbda8a31150
qodirjon <qodirjonsafaroff@gmail.com>
2022-07-13 18:03:13 +0500

    databases

--- app.py
+++ app.py
@@ -1,11 +1,14 @@
-from flask import Flask, render_template, request, redirect, url_for
-
+from flask import Flask, render_template, request, redirect, url_for, session
 from flask_wtf import FlaskForm
 from wtforms import StringField, PasswordField
 from wtforms.validators import InputRequired, AnyOf
+from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'something hard to guess'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///database.db'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+db = SQLAlchemy(app)
 
 
 class LoginForm(FlaskForm):
@@ -13,6 +16,27 @@ class LoginForm(FlaskForm):
     password = PasswordField('What is your password',
                              validators=[InputRequired(message='password-data required!'),
                                          AnyOf(['dev', 'ml'], message='wrong-password!')])
+
+
+class Role(db.Model):
+    __tablename__ = 'role'
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(50), nullable=False)
+    users = db.relationship('user', backref='role')
+
+    def __repr__(self):
+        return '<Role %r>' % self.id
+
+
+class User(db.Model):
+    __tablename__ = 'user'
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(120), nullable=False)
+    role = db.Column(db.Integer, db.ForeignKey('role.id'),
+                     nullable=False)
+
+    def __repr__(self):
+        return '<User %r>' % self.id
 
 
 @app.route('/')
@@ -28,6 +52,7 @@ def form():
     }
     if login_form.validate_on_submit():
         # login_form.username.data = ''
+        session['name'] = login_form.username.data
         return f'<h1>The username is {login_form.username.data}, the password is {login_form.password.data}</h1>'
     page_name = 'Login Form'
     return render_template("form.html", form_dict=form_dict, page_name=page_name)
